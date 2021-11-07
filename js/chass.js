fdc915d450457db9f4e0cebd1736e936c1bcdb51
Wellington N <42657376+Bryceed@users.noreply.github.com>
2021-11-07 15:46:41 -0300

    Interface fix
    - Rounded borders
    - Countdown (with animation)
    - White inner background filler
    - Checkers pieces

---
+++ js/chass.js
@@ -0,0 +1,16 @@
+/* ---------------------+
+|      CHESS SYSTEM     |
++----------------------*/
+
+var nowTurn = 0;
+var chessHash = 0;
+var
+
+
+function Board() {
+
+}
+
+document.addEventListener("click", event => {
+
+})
